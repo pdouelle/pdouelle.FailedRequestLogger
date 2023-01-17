66ab670d4c57bb5e87f6aa9b7938b3a3af0b2d81
Philippe Douelle <douelle.p@gmail.com>
2023-01-17 10:49:20 +0100

    remove serilog for Microsoft.Logging

--- pdouelle.FailedRequestLogger/FailedRequestLoggerHandler.cs
+++ pdouelle.FailedRequestLogger/FailedRequestLoggerHandler.cs
@@ -1,9 +1,16 @@
-﻿using Serilog;
+﻿using Microsoft.Extensions.Logging;
 
 namespace pdouelle.FailedRequestLogger;
 
 public sealed class FailedRequestLoggerHandler : DelegatingHandler
 {
+    private readonly ILogger<FailedRequestLoggerHandler> _logger;
+
+    public FailedRequestLoggerHandler(ILogger<FailedRequestLoggerHandler> logger)
+    {
+        _logger = logger;
+    }
+
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
@@ -11,7 +18,7 @@ public sealed class FailedRequestLoggerHandler : DelegatingHandler
         if (response.IsFailedStatusCode())
         {
             var content = await response.Content.ReadAsStringAsync(cancellationToken);
-            Log.Warning("Failed Request:{@Request}\nContent:{@Content}", request, content);
+            _logger.LogWarning("Failed Request:{@Request}\nContent:{@Content}", request, content);
         }
 
         return response;
